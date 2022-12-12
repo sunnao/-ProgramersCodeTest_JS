49cbe46a5f7942b1fe691f974e2977c743564fc8
sunnao <osa125125@naver.com>
2022-12-13 02:25:08 +0900

    221213

---
+++ 221214/문자열을 정수로 바꾸기.js
@@ -0,0 +1,12 @@
+// 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+
+function solution(s) {
+  return s*1;
+}
+
+/*
+function solution(s) {
+  return Number(s);
+}
+로 생각했는데, 문자열에 사칙연산을 하게 되면 숫자가 되는 방법이 있었음.
+*/
